b36855be8f26826e96c33a31c87000b2eec8fc22
Karl Ljungkvist <karl.ljungkvist@baesystems.se>
2022-12-13 16:47:28 +0100

    try to use a MinPrioHeap

--- day12/hs/main.hs
+++ day12/hs/main.hs
@@ -4,7 +4,12 @@ import System.IO
 import Control.Monad
 import Data.Char (ord, chr)
 import Data.List (elemIndex, delete)
+import qualified Data.Heap as H
+-- import qualified Data.Set as S  
+import qualified Data.HashSet as S
 import Data.Maybe
+
+type UnvisSet = S.HashSet Coord
 
 -- generic
 
@@ -82,42 +87,42 @@ getAccessibleNeighbors accF (Map hs _ _) n@(i,j) =
     fltr c = inside c && accF n c
     inside (ci,cj) = ci>=0 && cj>=0 && ci < length hs && cj < length (head hs)
 
-unvisitedNeighbors :: AccF -> Coord -> [Coord] -> Map -> [Coord]
+unvisitedNeighbors :: AccF -> Coord -> UnvisSet -> Map -> [Coord]
 unvisitedNeighbors accF n unvisited m =
-  filter (`elem` unvisited) $ getAccessibleNeighbors accF m n
+  filter (`S.member` unvisited) $ getAccessibleNeighbors accF m n
 
 -- showGrid :: Show a => [[a]] -> String
 -- showGrid = unlines . map show
 
--- traceGrid g = trace (showGrid g) g
+  
 
-dijkstra :: AccF -> Coord -> Coord -> [[Int]] -> [Coord] -> Map -> [[Int]]
-dijkstra accF tgt curr ds unvis m =
+-- traceGrid g = trace (showGrid g) g
+data DjkState = DjkState {accF :: AccF
+                         ,targetPos :: Coord
+                         ,currPos   :: Coord
+                         ,distances :: [[Int]]
+                         ,unvisited :: UnvisSet
+                         ,mapp      :: Map
+                         ,queue     :: H.MinPrioHeap Int Coord
+                         }
+
+dijkstra :: DjkState -> [[Int]]
+dijkstra s@(DjkState _ tgt curr ds unvis m q) =
   if tgt == nextCurr then
     nextDs
   else
-    dijkstra accF tgt nextCurr nextDs nextUnvis m
+    dijkstra s {currPos = nextCurr, distances = nextDs, unvisited = nextUnvis, queue = nextq}
   where
-    nbrs = unvisitedNeighbors accF curr unvis m
+    nbrs = unvisitedNeighbors (accF s) curr unvis m
     currD = ds!!!curr
     nextDs = foldl updateDistance ds nbrs
     updateDistance dsi nbr = setAt dsi nbr newDist
       where newDist = min (dsi!!!nbr) (currD+1)
 
-    nextCurr = foldl1 (\u1 u2 -> if (nextDs!!!u1) < (nextDs!!!u2) then u1 else u2) unvis
-    nextUnvis = delete nextCurr unvis
+    Just ((nD,nextCurr), nextq) = H.view nextq
+    nextUnvis = S.delete nextCurr unvis
 
 replicate2 n m v = replicate n (replicate m v)
-
-distMap m@(Map hs sp ep) =
-  distances
-  where
-    distances = dijkstra (accessibleFrom m) ep curr0 dist0 unvis0 m
-    curr0 = sp
-    dist0 = setAt (replicate2 rows cols (rows*cols)) sp 0
-    unvis0 = [(i,j) | i<-[0..rows-1], j<-[0..cols-1], (i,j)/=sp]
-    rows = length hs
-    cols = length $ head hs
 
 part1 :: [[Int]] -> Map -> Int
 part1 ds m =
@@ -134,12 +139,14 @@ getHeightPoints hs h =
 
 revDistMap :: Map -> [[Int]]
 revDistMap m@(Map hs sp ep) =
-  dijkstra accfun sp curr0 dist0 unvis0 m
+  dijkstra (DjkState accfun sp curr0 dist0 unvis0 m uvq0)
   where
     accfun s d = accessibleFrom m d s
     curr0 = ep
     dist0 = setAt (replicate2 rows cols (rows*cols)) ep 0
-    unvis0 = [(i,j) | i<-[0..rows-1], j<-[0..cols-1], (i,j)/=ep]
+    unvis0list = [(i,j) | i<-[0..rows-1], j<-[0..cols-1], (i,j)/=ep]
+    unvis0 = S.fromList unvis0list
+    uvq0 = H.fromList $ map (\c->(dist0!!!c,c)) unvis0list
     rows = length hs
     cols = length $ head hs
 
