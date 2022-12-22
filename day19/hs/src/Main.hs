35d48a4880d4dc56fdd9425eb5e0158293b7deb5
Karl Ljungkvist <k.ljungkvist@gmail.com>
2022-12-22 21:59:35 +0100

    try to do day19

--- day19/hs/src/Main.hs
+++ day19/hs/src/Main.hs
@@ -47,50 +47,45 @@ scaleVec4 (x1,x2,x3,x4) s = (x1*s,x2*s,x3*s,x4*s)
 
 lstFromVec4 (x1,x2,x3,x4) = [x1,x2,x3,x4]
 
-addDim (x1,x2,x3,x4) 0 n = (x1+n,x2,x3,x4)
-addDim (x1,x2,x3,x4) 1 n = (x1,x2+n,x3,x4)
-addDim (x1,x2,x3,x4) 2 n = (x1,x2,x3+n,x4)
-addDim (x1,x2,x3,x4) 3 n = (x1,x2,x3,x4+n)
+incDim :: Vec4 -> Int -> Vec4
+incDim (x1,x2,x3,x4) 0 = (x1+1,x2,x3,x4)
+incDim (x1,x2,x3,x4) 1 = (x1,x2+1,x3,x4)
+incDim (x1,x2,x3,x4) 2 = (x1,x2,x3+1,x4)
+incDim (x1,x2,x3,x4) _ = (x1,x2,x3,x4+1)
 
 affords :: Vec4 -> Vec4 -> Bool
 affords (r1,r2,r3,r4) (c1,c2,c3,c4) = c1<=r1 && c2<=r2 && c3<=r3 && c4<=r4
 
-nAffords :: Vec4 -> Vec4 -> Int
-nAffords r c =
-  trace (show r ++ " " ++ show c ++ "  ->  " ++ show banan) banan
-  where minMax mm (rr,cc) =
-          if cc == 0 then mm
-          else min mm (rr `div` cc)
-        banan = foldl minMax 10000 $ zip (lstFromVec4 r) (lstFromVec4 c)
+geodeCount (ProdState (_,_,_,g1) _) = g1
 
-purchaseOptions :: Vec4 -> Blueprint -> [Vec4]
-purchaseOptions res bp =
-  map snd $ foldl getOpts [(res,(0,0,0,0))] $ zip [0..] bp
-  where
-    getOpts opts (i,price) =  concatMap (expandOpts i price) opts
-    expandOpts i price (resLeft,opt) = map (\nbuy -> (subScale resLeft nbuy price, addDim opt i nbuy)) [0..(nAffords resLeft opt)]
-    subScale x a y = x `subVec4` (scaleVec4 y a)
-
-orderStates :: ProdState -> ProdState -> Ordering
-orderStates (ProdState (_,_,_,g1) _) (ProdState (_,_,_,g2) _) = g1 `compare` g2
-
-computeCost :: Blueprint -> Vec4 -> Vec4
-computeCost bp po = foldl1 addVec4 $ zipWith scaleVec4  bp (lstFromVec4 po)
+orderStates s1 s2 = (geodeCount s1) `compare` (geodeCount s2)
 
 stepTime :: Blueprint -> Int -> ProdState -> ProdState
-stepTime bp 0 state = state
+stepTime _ 0 state = state
 stepTime bp t state =
-  maximumBy orderStates finalStates
+  maximumBy orderStates $ map procOpt options
+
   where
     res = resourceCount state
     robs = robotCount state
-    -- nextStates = mstate]
-    genNextState po = state { robotCount = robs `addVec4` po, resourceCount = res `addVec4` robs `subVec4` computeCost bp po }
-    finalStates = map (stepTime bp (t-1).genNextState) (purchaseOptions res bp)
 
+    options = (res,robs):[(res `subVec4` cost, incDim robs i) | (cost,i) <-zip bp [0..], affords res cost]
+    procOpt (rs,rb) = stepTime bp (t-1) state { robotCount = rb, resourceCount = rs `addVec4` robs }
+
+maxGeode :: Blueprint -> Int -> ProdState -> Int
+maxGeode _ 0 state = geodeCount state
+maxGeode bp t state =
+  maximum $ map procOpt options
+  where
+    res = resourceCount state
+    robs = robotCount state
+
+    options = (res,robs):[(res `subVec4` cost, incDim robs i) | (cost,i) <-zip bp [0..], affords res cost]
+    procOpt (rs,rb) = maxGeode bp (t-1) state { robotCount = rb, resourceCount = rs `addVec4` robs }
 
 computeMaxGeodes bp =
-  stepTime bp 1 ProdState { resourceCount = (0,0,0,0)
+  -- stepTime bp 20 ProdState { resourceCount = (0,0,0,0)
+  maxGeode bp 24 ProdState { resourceCount = (0,0,0,0)
                            , robotCount = (1,0,0,0)
                            }
 
