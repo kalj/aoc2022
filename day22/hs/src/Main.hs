3f8a127b9a15657e08e0b22a0813361323f00716
Karl Ljungkvist <k.ljungkvist@gmail.com>
2022-12-25 21:51:38 +0100

    Do day22 part1

--- day22/hs/src/Main.hs
+++ day22/hs/src/Main.hs
@@ -40,22 +40,43 @@ parseStuff contents =
 type Grid = [String]
 type Coord = (Int,Int)
 
-
 data State = State { gridMap   :: Grid
                    , position  :: Coord
                    , direction :: Int
                    }
-  deriving(Show)
+  -- deriving(Show)
+instance Show State
+  where show s = "pos=" ++ show (position s) ++ "\n" ++
+                 "dir=" ++ show (direction s) ++ "\n" ++
+                 unlines (zipWith (\row r-> zipWith (renderPos r) row [0..]) (gridMap s) [0..])
+          where
+            renderPos r v c = if (r,c)==position s then
+                                case direction s of
+                                  0 -> '>'
+                                  1 -> 'v'
+                                  2 -> '<'
+                                  _ -> '^'
+                              else v
+
 
 at :: State -> Coord -> Char
-at s c = (gridMap s)!!(fst c)!!(snd c)
+at s crd = (gridMap s)!!(fst crd)!!(snd crd)
+
+isOutside :: State -> Coord -> Bool
+isOutside s (r,c) = r<0 || r>=(length gd) || c<0 || c>= length (gd!!r)
+  where gd = gridMap s
 
 
+revFindIndex :: (a -> Bool) -> [a] -> Maybe Int
+revFindIndex f xs = fmap ((length xs - 1) - )  (findIndex f (reverse xs))
+
+wrapPos :: State -> Coord -> Coord
 wrapPos s (r,c) = case direction s of
-  0 -> (r,findIndex (' '/=) (gridMap s!!r)
-  1 -> (r,findIndex (' '/=) (gridMap s!!r)
-  2 -> (r,findIndex (' '/=) (gridMap s!!r)
-  _ -> (r,findIndex (' '/=) (gridMap s!!r)
+                    0 -> (r,fromJust $ findIndex (' '/=) (gridMap s!!r))
+                    1 -> (fromJust (findIndex matchRow (gridMap s)), c)
+                    2 -> (r, fromJust $ revFindIndex (' '/=) (gridMap s!!r))
+                    _ -> (fromJust (revFindIndex matchRow (gridMap s)), c)
+  where matchRow row = c < length row && row!!c /= ' '
 
 takeStep :: State -> State
 takeStep s = s { position = newPos }
@@ -63,22 +84,26 @@ takeStep s = s { position = newPos }
     dir = direction s
     oldPos@(r,c) = position s
     newPos = if s`at`movePos == '.' then movePos else oldPos
-    movePos = if s`at`tgtPos == ' ' then wrapPos dir tgtPos else tgtPos
+    movePos = if isOutside s tgtPos || s`at`tgtPos == ' ' then wrapPos s tgtPos else tgtPos
     tgtPos = case dir of
-               0 -> (r+1,c)
-               1 -> (r,c+1)
-               2 -> (r-1,c)
-               _ -> (r,c-1)
+               0 -> (r,c+1)
+               1 -> (r+1,c)
+               2 -> (r,c-1)
+               _ -> (r-1,c)
 
 doAction :: State -> Action -> State
 doAction s TurnR        = s { direction = (direction s + 1) `mod` 4 }
 doAction s TurnL        = s { direction = (direction s - 1) `mod` 4 }
 doAction s (Move steps) = (!!steps) $ iterate takeStep s
 
--- part1 :: String -> Int
+computePassword :: State -> Int
+computePassword (State _ (r,c) f) = 1000*(r+1)+4*(c+1)+f
+
+part1 :: String -> Int
 part1 contents =
-  foldl doAction State { gridMap = grid, position = startPos, direction = 0 } actions
+  computePassword finalState
   where
+    finalState = foldl doAction State { gridMap = grid, position = startPos, direction = 0 } actions
     startPos = (0,fromJust (elemIndex '.' firstRow))
     firstRow = head grid
     (grid,actions) = parseStuff contents
